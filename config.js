c4069717a3bcd57f4afa1619f03304b691b98419
Michael Bosik <michaelbosik@Michaels-MacBook-Air.local>
2026-03-10 14:09:13 -0400

    added sports calendar

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const CONFIG = {
     ACMI_CALENDAR_ID:
       "c_e19c5b2f73f36390a89470c28a7b097b64e4eb055f8cccbabe2b523994747900@group.calendar.google.com",
     HOLIDAY_CALENDAR_ID: "en.usa#holiday@group.v.calendar.google.com",
+    SPORTS_CALENDAR_ID: "c_bh5arljqqeubqreqjjgg1lts98@group.calendar.google.com"
   },
   WORDNIK: ""
 };
