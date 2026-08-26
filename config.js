309c4066ae70db642bbcb01b54fa0a47918a68f9
Michael Bosik <michaelbosik@gmail.com>
2026-08-26 14:45:34 -0400

    no youth calendar

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const CONFIG = {
     ACMI_CALENDAR_ID:
       "c_e19c5b2f73f36390a89470c28a7b097b64e4eb055f8cccbabe2b523994747900@group.calendar.google.com",
     HOLIDAY_CALENDAR_ID: "en.usa#holiday@group.v.calendar.google.com",
-    SPORTS_CALENDAR_ID:
-      "c_bh5arljqqeubqreqjjgg1lts98@group.calendar.google.com",
+    // SPORTS_CALENDAR_ID:
+    //   "c_bh5arljqqeubqreqjjgg1lts98@group.calendar.google.com",
   },
   EVENTS_SHEET_ID: "1xa5sya5jADtJ_wGiYU1lV06CVOYfKmYKUnenKqMJWvE",
   EVENTS_SHEET_RANGE: "CrewCalls!A2:E",
